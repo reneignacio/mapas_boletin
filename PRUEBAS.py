06617ff79aeb5c8bf297811525217cc7a9ee0e70
rene <reneignaciosepulvedajara@gmail.com>
2023-08-21 11:55:16 -0400

    fix read layers

--- PRUEBAS.py
+++ PRUEBAS.py
@@ -20,34 +20,42 @@ regiones = {
     "R16": "Región del Ñuble"
 }
 savi=0
-import arcpy
-import os
 mxd = arcpy.mapping.MapDocument("C:\\Users\\INIA\\Desktop\\MAPA_ANOMALIA_PYTHON\ANOMALIA_SAVI.mxd")
 df = arcpy.mapping.ListDataFrames(mxd)[0]
-layers = arcpy.mapping.ListLayers(mxd)[0]
-
+layers = arcpy.mapping.ListLayers(mxd)
+layers
 
 #Cargar capa SAVI.tif
 for region in regiones.keys():
-    R=region
-    ruta_anomalia_SAVI_tif="C:\\Users\\INIA\\Desktop\\2023-06-26_v2\\{0}\\TIF\\ANOMALIA\\{0}_Anomalia_SAVI_median_ZA.tif".format(R)
+    R = region
+    ruta_anomalia_SAVI_tif = "C:\\Users\\INIA\\Desktop\\2023-06-26_v2\\{0}\\TIF\\ANOMALIA\\{0}_Anomalia_SAVI_median_ZA.tif".format(R)
     if os.path.exists(ruta_anomalia_SAVI_tif):
-        capa_anomalia=arcpy.mapping.Layer(ruta_anomalia_SAVI_tif)
-        arcpy.mapping.AddLayer(df,capa_anomalia,"BOTTOM")
-        print("tif region {} añadido".format(R))
-        savi=savi+1        
-if savi==3:
-    print("se subieron {} capas de SAVI, esta correcto".format(savi))
-else: 
-    print("el numero de capas SAVI subidas es {} y deberian ser 3".format(savi))
+        try:
+            capa_anomalia = arcpy.mapping.Layer(ruta_anomalia_SAVI_tif)
+            arcpy.mapping.AddLayer(df, capa_anomalia, "BOTTOM")
+            print("tif {} añadido".format(R))
+            savi += 1
+        except Exception as e:
+            print("Error cargando capa para la region {}: {}".format(R, e))
 
+if savi == 3:
+    print("se detectaron {} capas de SAVI, esta correcto".format(savi))
+else:
+    print("el numero de capas SAVI es {} y deberian ser 3".format(savi))
+
+
+mxd.save()
+arcpy.RefreshActiveView()
+arcpy.RefreshTOC()
 
 
 #MEJOR CAMBIARLO POR ALGO ASI, QUE REVISA LAS CAPAS DENTRO DEL MXD
 
 # Extraer solo los nombres y ordenar la lista alfabéticamente
 layer_names = sorted([layer.name for layer in layers])
+layer_names
 # Filtrar y contar los nombres de las capas que contienen "Anomalia_SAVI"
+
 anomalia_savi_count = len([name for name in layer_names if "Anomalia_SAVI" in name])
 print(anomalia_savi_count)
 
