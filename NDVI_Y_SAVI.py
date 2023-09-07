efc91fbc5b79032b3acfd57d31defdcc548506af
reneignacio <reneignaciosepulvedajara@gmail.com>
2023-09-07 12:08:31 -0300

    correccion de nombres archivos  NDVI y SAVI

--- NDVI_Y_SAVI.py
+++ NDVI_Y_SAVI.py
@@ -22,7 +22,7 @@ regiones = {
 #os.chdir("D:/mapas_boletin/mapas_boletin")
 veg_index_=["NDVI","SAVI"]
 fecha="26 de junio al 11 de julio de 2023"
-mxd = arcpy.mapping.MapDocument("NDVI_horizontal.mxd")
+mxd = arcpy.mapping.MapDocument("NDVI_y_SAVI.mxd")
 df = arcpy.mapping.ListDataFrames(mxd)[0]
 df0 = arcpy.mapping.ListDataFrames(mxd)[0]
 df1=arcpy.mapping.ListDataFrames(mxd)[1]
