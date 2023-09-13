e0cb02529f4bede45337fea6622afe2a3ea5c0ce
reneignacio <reneignaciosepulvedajara@gmail.com>
2023-09-13 19:02:21 -0300

    update

--- run_all.py
+++ run_all.py
@@ -5,5 +5,7 @@ os.chdir("C:/Users/Marcel/Desktop/mapas_boletin/mapas_boletin/")
 os.system('python anom_NDVI_Y_SAVI.py &')
 os.system('python dif_NDVI_Y_SAVI.py &')
 os.system('python NDVI_Y_SAVI.py &')
-os.system('VCI.py &')
-os.system('soil_moisture.py &')
+os.system('python VCI.py &')
+#os.system('python soil_moisture.py &')
+
+#se demora aprox 15 min
