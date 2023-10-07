64c1cb9ca8ef03ee3f924bca8a1fefe49f091a7f
reneignacio <reneignaciosepulvedajara@gmail.com>
2023-10-07 00:52:34 -0300

    mapas octubre creados

--- run_all.py
+++ run_all.py
@@ -1,13 +1,14 @@
 import os
 import arcpy
 
-#os.chdir("C:/Users/Marcel/Desktop/mapas_boletin/mapas_boletin/")
-os.chdir("D:/INIA/mapas_boletin/mapas_boletin/")
+os.chdir("C:/Users/Marcel/Desktop/mapas_boletin/mapas_boletin/")
+#os.chdir("D:/INIA/mapas_boletin/mapas_boletin/")
 os.system('python anom_NDVI_Y_SAVI.py &')
 os.system('python dif_NDVI_Y_SAVI.py &')
-os.system('python NDVI_Y_SAVI.py &')
 os.system('python VCI.py &')
+os.system('python NDVI_Y_SAVI.py &')
 #os.system('python soil_moisture.py &')
+
 
 #se demora aprox 8 min
 #resultados se guardan en carpeta "export"
